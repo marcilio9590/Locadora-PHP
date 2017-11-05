321cabdad8fc2a301938b80899d8245857f4cb3c
marcilio9590 <marcilio9590@gmail.com>
2017-11-05 17:54:26 -0200

    format

--- views/locacao.php
+++ views/locacao.php
@@ -49,15 +49,7 @@
                         </table>
                     </div>
                     <div class="col-sm-2"></div>
-                    
                 </div>
-
-
-
-
-
-
-
         </div>
     </body>
 </html>

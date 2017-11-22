9ebbedaebefa71b44d80dc3ddf06023268661501
claudio ventura <claudiomildo@gmail.com>
2017-11-21 23:36:50 -0200

    Realizei alterações no meu controller, para realizar consultas na base de locações, clientes e filmes. No menu view da parte e funcionário, realizei liguação dos links, vou tentar terminar a minha parte e ajudar no que for preciso o programador da parte de funcionário, pois encontra-se em atraso.

--- views/menu.php
+++ views/menu.php
@@ -3,14 +3,14 @@
     if($endereco == "/Locadora-PHP/" || $endereco == "/Locadora-PHP/index.php"){
       $index = "index.php";
       $urlCliente = "views/cliente.php";
-      $urlFuncionario = "#";
+      $urlFuncionario = "views/funcionario";
       $urlFilmes = "views/filme.php";
       $urlLocacao = "views/locacao.php";
-      $urlDevolucao = "views/devolucao";
+      $urlDevolucao = "views/devolucao.php";
     }else{
       $index = "../index.php";
       $urlCliente = "../views/cliente.php";
-      $urlFuncionario = "#";
+      $urlFuncionario = "../views/funcionario";
       $urlFilmes = "../views/filme.php";
       $urlLocacao = "../views/locacao.php";
       $urlDevolucao = "../views/devolucao.php";
@@ -33,7 +33,7 @@
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
               <li><a href="<?php echo $urlCliente ?>">Clientes</a></li>
-              <li><a href="#">Funcionários</a></li>
+              <li><a href="<?php echo $urlFuncionario?>">Funcionários</a></li>
               <li><a href="<?php echo $urlFilmes ?>">Filmes</a></li>
               <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Operações<span class="caret"></span></a>
